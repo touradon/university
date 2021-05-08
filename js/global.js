f0a6df88178f2ec4dfc78a95f6f40799a33b73a3
touradon <1240928910@qq.com>
2021-05-08 17:50:47 +0800

    y

--- js/global.js
+++ js/global.js
@@ -1,19 +1,11 @@
-//header_home
-function autoheader(){
-	if($(".header").length>0 && $(".header2").length>0 && $(".home").length==0){
-		if($(window).scrollTop()>$(".banner_frame").height()-$(".header").height()){
-			//$(".header").removeClass("dark").addClass("light");
-			$(".header").removeClass("showdiv");
-			$(".header2").addClass("showdiv");
+function autoheader() {
+	if ( $(window).scrollTop() > $(".banner").height() - $(".ld-header").height()) {
+			$(".ld-header").addClass("showdiv");
 		}
-		else{
-			//$(".header").addClass("dark").removeClass("light");
-			$(".header2").removeClass("showdiv");
-			$(".header").addClass("showdiv");
+		else {
+			$(".ld-header").removeClass("showdiv");
 		}
-	}
 }
-
 $(window).scroll(function(){
 	autoheader();	
 })
